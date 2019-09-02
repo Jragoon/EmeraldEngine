55e8be41f651a84d40ad2de4a1cc3b804abca7b5
Jared Taylor <jared.xyz@mailbox.org>
2019-09-02 13:00:34 -0400

    Jumping states

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -10,9 +10,14 @@ import static org.lwjgl.glfw.GLFW.*;
 public class Player extends Entity {
 	private static final float MOVEMENT_SPEED = .1f;
 	private static final float TURN_SPEED = .2f;
+	private static final float GRAVITY = -.001f;
+	private static final float JUMP = .3f;
+	private static final float TERRAIN_HEIGHT = 0;
 
 	private float speed = 0;
 	private float turnSpeed = 0;
+	private float upwardsSpeed = 0;
+	private boolean isJumping = false;
 
 	public Player(TexturedModel model, Vector3f pos, float rotX, float rotY, float rotZ, float scale) {
 		super(model, pos, rotX, rotY, rotZ, scale);
@@ -25,6 +30,13 @@ public class Player extends Entity {
 		float dx = distance * (float) Math.sin(Math.toRadians(super.getRotationY()));
 		float dz = distance * (float) Math.cos(Math.toRadians(super.getRotationY()));
 		super.increasePosition(dx, 0, dz);
+		upwardsSpeed += GRAVITY * getFrameTimeMillis();
+		super.increasePosition(0, upwardsSpeed * getFrameTimeMillis(), 0);
+		if (super.position.y < TERRAIN_HEIGHT) {
+			upwardsSpeed = 0;
+			super.position.y = TERRAIN_HEIGHT;
+			isJumping = false;
+		}
 	}
 
 	private void getMovements() {
@@ -41,6 +53,10 @@ public class Player extends Entity {
 		}
 		if (keyIsPressed(GLFW_KEY_D)) {
 			this.turnSpeed -= TURN_SPEED;
+		}
+		if (keyIsPressed(GLFW_KEY_SPACE) && !isJumping) {
+			this.upwardsSpeed = JUMP;
+			isJumping = true;
 		}
 	}
 }
